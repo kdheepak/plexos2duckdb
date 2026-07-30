ed97dc6e32c644e7327d1cfcce616374a341bf6f
Dheepak Krishnamurthy <1813121+kdheepak@users.noreply.github.com>
2026-07-30 18:44:07 -0400

    chore: Update dependencies

--- build.rs
+++ build.rs
@@ -5,14 +5,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     // NOTE: This will output everything, and requires all features enabled.
     // NOTE: See the specific builder documentation for configuration options.
-    let build = vergen_gitcl::BuildBuilder::all_build()?;
-    let cargo = vergen_gitcl::CargoBuilder::all_cargo()?;
-    let gitcl = vergen_gitcl::GitclBuilder::default()
-        .all()
-        .describe(false, true, None)
-        .build()?;
-    let rustc = vergen_gitcl::RustcBuilder::all_rustc()?;
-    let si = vergen_gitcl::SysinfoBuilder::all_sysinfo()?;
+    let build = vergen_gitcl::Build::all_build();
+    let cargo = vergen_gitcl::Cargo::all_cargo();
+    let gitcl = vergen_gitcl::Gitcl::all_git();
+    let rustc = vergen_gitcl::Rustc::all_rustc();
+    let si = vergen_gitcl::Sysinfo::all_sysinfo();
 
     vergen_gitcl::Emitter::default()
         .add_instructions(&build)?
